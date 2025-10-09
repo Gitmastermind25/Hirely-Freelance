b30177cae066fd0a0c18acf7f2cac2b15f4fe287
Yogita <yogitapandit2505@gmail.com>
2025-10-09 16:53:23 +0530

    Add files via upload

---
+++ api/routes/user.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { getUser,deleteUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../middleware/jwt.js';
+
+const router = express.Router();
+
+
+router.get("/:id",getUser)
+router.delete("/:id",verifyToken ,deleteUser)
+
+
+
+export default router;  
